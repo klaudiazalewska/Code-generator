96a86faf12d5359ed4c055ac77307c7d1ca15f55
klaudiazalewska <73396554+klaudiazalewska@users.noreply.github.com>
2022-06-01 20:13:38 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,75 @@
+const btn = document.querySelector('button');
+const section = document.querySelector('section');
+const chars = 'ABCDEFGH0123456789';
+
+const codesNumber = 1000;
+const charsNumber = 12;
+
+const codesGenerator = () => {
+    for (let i = 0; i < codesNumber; i++) {
+        let code = "";
+        for (let i = 0; i < charsNumber; i++) {
+            const index = Math.floor(Math.random() * chars.length);
+            code += chars[index];
+        }
+    const div = document.createElement('div');
+    div.textContent = code;
+    section.appendChild(div);
+    }
+}
+
+btn.addEventListener('click', codesGenerator);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const chars = 'ABCDEFGIJK0123456789';
+
+// const btn = document.querySelector('button');
+// const section = document.querySelector('section');
+
+// const codesNumber = 1000;
+// const charsNumber = 12;
+
+// const codesGeneretor = () => {
+//  for (let i = 0; i < codesNumber; i++) {
+//   let code = "";
+//   for (let i = 0; i < charsNumber; i++) {
+//    const index = Math.floor(Math.random() * 20)
+//    code += chars[index];
+//   }
+//   const div = document.createElement('div');
+//   div.textContent = code;
+//   section.appendChild(div)
+//  }
+// }
+
+// btn.addEventListener('click', codesGeneretor)
